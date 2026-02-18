9f48a9933f33452d68e99738c51eac5d1df99e01
zaheer <info.zaheerjk@gmail.com>
2026-02-18 13:59:07 +0530

    entire code

---
+++ Attendence/__init__.py
@@ -0,0 +1,10 @@
+# Attendence/__init__.py
+"""
+Attendence package initializer.
+"""
+import logging
+logging.captureWarnings(True)
+
+from .admin import show_admin_panel
+from .analytics import show_analytics_panel
+from .student import show_student_panel
